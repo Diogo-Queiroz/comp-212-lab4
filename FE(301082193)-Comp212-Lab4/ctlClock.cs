306bbd703a6ccecbec82c1d637efbbafbe75f530
Diogo Queiroz <diogoqueirtooz1212@gmail.com>
2020-02-11 16:33:05 -0500

    Add project files.

---
+++ FE(301082193)-Comp212-Lab4/ctlClock.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FE_301082193__Comp212_Lab4
+{
+    public partial class ctlClock: UserControl
+    {
+        public ctlClock()
+        {
+            InitializeComponent();
+        }
+    }
+}
